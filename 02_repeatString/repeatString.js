ccdd89639fbb7b973d599bb316179ba22e6452db
Oscar Soanes <oscarsoanes@gmail.com>
2022-11-12 22:31:06 +0000

    modified code to pass test case

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(message, times) {
+    let output = "";
 
+    if (times < 0) {
+        output += "ERROR";
+        return output;
+    }
+
+    for (i = 0; i < times; i++) {
+        output += message
+    }
+    return output
 };
 
 // Do not edit below this line
