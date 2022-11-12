5ee681c2e58a61c045c7a6d7349feccb06d616e0
Oscar Soanes <oscarsoanes@gmail.com>
2022-11-12 23:36:12 +0000

    Added remove from array code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (list = [], ...inputs) {
 
+    for (const input of inputs) {
+        for (let i = 0; i < list.length; i++) {
+            if (typeof (list[i]) !== typeof (input)) {
+                continue;
+            }
+
+            if (list[i].toString() === input.toString()) {
+                list.splice(i, 1);
+            }
+        }
+    }
+
+
+
+    return list;
 };
 
 // Do not edit below this line
