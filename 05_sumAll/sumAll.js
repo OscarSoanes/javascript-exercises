c046de05eb5bbb85203fff957bc82088e8c6381c
Oscar Soanes <oscarsoanes@gmail.com>
2022-11-12 23:48:39 +0000

    created sum all to pass tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let output = 0;
 
+    // switch start and end if start is larger
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    if (start < 0 || end < 0 || typeof(start) != "number" || typeof(end) != "number") {
+        return "ERROR";
+    }
+
+
+    for (let i = start; i <= end; i++) {
+        output += i;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
