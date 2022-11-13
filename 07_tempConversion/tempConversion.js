b32341989519fe61e4b6ffcc1bc2de853d7fd1dd
Oscar Soanes <oscarsoanes@gmail.com>
2022-11-13 00:07:04 +0000

    temp conversion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  // -32 * 5/9
+  let output = (fahrenheit - 32) * (5/9);
 
+  return Math.round(output * 10) / 10;
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+  // * 1.8 + 32
+  let output = (celsius * 9/5) + 32;
 
+  return Math.round(output * 10) / 10;
 };
 
 // Do not edit below this line
