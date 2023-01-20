7975391c61eda8cd9e5c15f0fccbd362ee26a99f
Oscar Soanes <oscarsoanes@gmail.com>
2023-01-20 19:37:57 +0000

    fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(stopper) {
+    
+    stopper = parseInt(stopper);
+    if (stopper < 0) {
+        return "OOPS";
+    }
+    
+    let current = 1;
+    let previous = 0;
+    let next;
 
+    if (stopper == 1) {
+        return current;
+    }
+    // greater than 1
+    for (var index = 2; index <= stopper; index++) {
+        next = current + previous;
+        previous = current;
+        current = next;
+    }
+    return next;
 };
 
 // Do not edit below this line
