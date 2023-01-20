357669c69f7a518d411a3e1709b31941bd0bac20
Oscar Soanes <oscarsoanes@gmail.com>
2023-01-20 20:23:20 +0000

    functions working with correct data output

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(element => {
+        if (element.yearOfDeath == undefined) {
+            element.yearOfDeath = new Date().getFullYear();
+        }
+    });
 
+    const sorted = people.sort((a, b) => 
+        a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1 : -1
+    )
+
+    return sorted[0]
 };
 
 // Do not edit below this line
