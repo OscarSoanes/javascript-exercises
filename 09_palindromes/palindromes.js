f8d1faf9fdfbced74a4fef75134c04dcf06e60c7
Oscar Soanes <oscarsoanes@gmail.com>
2023-01-20 19:08:37 +0000

    core palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (message) {
+    var messageReversed = "";
 
+    for (let index = message.length; index >= 0; index--) {
+        messageReversed += message.charAt(index)
+    }
+
+    if (message == messageReversed) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
